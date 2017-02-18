d508ca7c372b22e421dbe92655e32d7e03a13fb0
Michael Andrew <michael@gladeye.co.nz>
2017-02-18 21:35:25 +1300

    Fixes to handle error API responses
    Now handles instances where no correct download link is returned or where API rate limiting kicks in. Automatically retries up to X times, as specified by `DOWNLOAD_MAX_RETRIES` value in `configuration.conf`

--- minecraft-realms-map.sh
+++ minecraft-realms-map.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+trap "exit 1" TERM
+export TOP_PID=$$
+
 AUTH_URL="https://authserver.mojang.com/authenticate"
 REFRESH_URL="https://authserver.mojang.com/refresh"
 WORLD_URL="https://mcoapi.minecraft.net/worlds"
 
+DATE=`date +%Y-%m-%d`
+FILE_PATH="backups/mcr_world_$DATE.tar.gz"
+RETRIES=1
+
+function exitNow {
+    kill -s TERM $TOP_PID
+}
 function getFormat {
     SEPARATOR=$1
     echo `date "+%Y-%m-%d_%H$SEPARATOR%M$SEPARATOR%S"`
@@ -14,60 +24,103 @@ function log {
    echo "[$DATE] $1"
 }
 
-DATE=$(getFormat ".")
-exec > >(tee -i logs/map-generation-"$DATE".log)
+function downloadWorld {
+
+    if [ "$RETRIES" -gt "$DOWNLOAD_MAX_RETRIES" ]
+        then
+        log "Exceeded download retries count. Aborting...."
+        exitNow
+    fi
+
+    log "Getting Minecraft authentication credentials..."
+
+    #get auth server temp access token
+    RESPONSE1=$(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{
+        \"requestUser\": true,
+        \"agent\":\"Agent{name='Minecraft', version=1}\",
+        \"username\":\"$MINECRAFT_USERNAME\",
+        \"clientToken\":\"$MINECRAFT_CLIENT_ID\",
+        \"password\":\"$MINECRAFT_PASSWORD\"
+    }" "$AUTH_URL")
+
+    //log "$RESPONSE1"
+
+    #parse and store temp access token
+    TEMP_ACCESS_TOKEN=$(echo $RESPONSE1 | jq -r '.accessToken')
+
+    #get auth server long term access token
+    RESPONSE2=$(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{
+        \"requestUser\": true,
+        \"accessToken\":\"$TEMP_ACCESS_TOKEN\",
+        \"clientToken\":\"$MINECRAFT_CLIENT_ID\",
+        \"selectedProfile\":{
+            \"id\": \"$MINECRAFT_PROFILE_ID\"
+        }
+    }" "$REFRESH_URL")
+
+    //log "$RESPONSE2"
+
+    #parse and store long term access token
+    LONG_ACCESS_TOKEN=$(echo $RESPONSE2 | jq -r '.accessToken')
+    MINECRAFT_USER=$(echo $RESPONSE2 | jq -r '.selectedProfile.name')
+
+    log "Getting temporary download link for world backup..."
+
+    #get Realms server ID
+    RESPONSE3=$(curl -s -X GET -H "Content-Type: application/json" -H "Cookie: sid=token:$LONG_ACCESS_TOKEN:$MINECRAFT_PROFILE_ID;user=$MINECRAFT_USER;version=$MINECRAFT_VERSION" -H "Cache-Control: no-cache" -H "Pragma: no-cache" "$WORLD_URL")
+
+    //log "$RESPONSE3"
+
+    #parse and store server ID
+    SERVER_ID=$(echo $RESPONSE3 | jq -r '.servers[0].id')
+    DAYS_LEFT=$(echo $RESPONSE3 | jq -r '.servers[0].daysLeft')
+
+    log "$DAYS_LEFT days remain for the current Realms subscription."
+
+    #get world backup URL
+    RESPONSE4=$(curl -s -X GET -H "Content-Type: application/json" -H "Cookie: sid=token:$LONG_ACCESS_TOKEN:$MINECRAFT_PROFILE_ID;user=$MINECRAFT_USER;version=$MINECRAFT_VERSION" -H "Cache-Control: no-cache" -H "Pragma: no-cache" "$WORLD_URL/$SERVER_ID/slot/$MINECRAFT_WORLD_NUM/download")
+
+    //log "$RESPONSE4"
+
+    if [ "$RESPONSE4" == 'Retry again later' ]
+        then
+            log "Couldn't get download link as API asked to retry again later. Waiting 30 seconds then restarting download $RETRIES/$DOWNLOAD_MAX_RETRIES..."
+            RETRIES=$[RETRIES + 1]
+            sleep 30
+            downloadWorld
+    fi
+
+    DOWNLOAD_LINK=$(echo $RESPONSE4 | jq -r '.downloadLink')
+
+    if [ -z "$DOWNLOAD_LINK" ]
+        then
+            log "Couldn't get download link. Restarting download $RETRIES/$DOWNLOAD_MAX_RETRIES..."
+            RETRIES=$[RETRIES + 1]
+            downloadWorld
+    fi
+
+    log "Downloading world backup from $DOWNLOAD_LINK. This may take several minutes..."
+    wget "$DOWNLOAD_LINK" -O "$FILE_PATH"
+
+    gunzip -c file.tar.gz | tar t > /dev/null;
+
+    if [ $? -ne 0 ]
+        then
+            log "World file is corrupt. Restarting download $RETRIES/$DOWNLOAD_MAX_RETRIES..."
+            RETRIES=$[RETRIES + 1]
+            downloadWorld
+    fi
+
+}
+
+NEW_DATE=$(getFormat ".")
+exec > >(tee -i ./logs/map-generation-"$NEW_DATE".log)
 exec 2>&1
 
 #get config values
 source configuration.conf
 
-log "Getting Minecraft authentication credentials..."
-
-#get auth server temp access token
-RESPONSE1=$(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{
-    \"requestUser\": true,
-    \"agent\":\"Agent{name='Minecraft', version=1}\",
-    \"username\":\"$MINECRAFT_USERNAME\",
-    \"clientToken\":\"$MINECRAFT_CLIENT_ID\",
-    \"password\":\"$MINECRAFT_PASSWORD\"
-}" "$AUTH_URL")
-
-#parse and store temp access token
-TEMP_ACCESS_TOKEN=$(echo $RESPONSE1 | jq -r '.accessToken')
-
-#get auth server long term access token
-RESPONSE2=$(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{
-    \"requestUser\": true,
-    \"accessToken\":\"$TEMP_ACCESS_TOKEN\",
-    \"clientToken\":\"$MINECRAFT_CLIENT_ID\",
-    \"selectedProfile\":{
-        \"id\": \"$MINECRAFT_PROFILE_ID\"
-    }
-}" "$REFRESH_URL")
-
-#parse and store long term access token
-LONG_ACCESS_TOKEN=$(echo $RESPONSE2 | jq -r '.accessToken')
-MINECRAFT_USER=$(echo $RESPONSE2 | jq -r '.selectedProfile.name')
-
-log "Getting temporary download link for world backup..."
-
-#get Realms server ID
-RESPONSE3=$(curl -s -X GET -H "Content-Type: application/json" -H "Cookie: sid=token:$LONG_ACCESS_TOKEN:$MINECRAFT_PROFILE_ID;user=$MINECRAFT_USER;version=$MINECRAFT_VERSION" -H "Cache-Control: no-cache" -H "Pragma: no-cache" "$WORLD_URL")
-
-#parse and store server ID
-SERVER_ID=$(echo $RESPONSE3 | jq -r '.servers[0].id')
-DAYS_LEFT=$(echo $RESPONSE3 | jq -r '.servers[0].daysLeft')
-
-#get world backup URL
-RESPONSE4=$(curl -s -X GET -H "Content-Type: application/json" -H "Cookie: sid=token:$LONG_ACCESS_TOKEN:$MINECRAFT_PROFILE_ID;user=$MINECRAFT_USER;version=$MINECRAFT_VERSION" -H "Cache-Control: no-cache" -H "Pragma: no-cache" "$WORLD_URL/$SERVER_ID/slot/$MINECRAFT_WORLD_NUM/download")
-
-DOWNLOAD_LINK=$(echo $RESPONSE4 | jq -r '.downloadLink')
-
-DATE=`date +%Y-%m-%d`
-FILE_PATH="backups/mcr_world_$DATE.tar.gz"
-
-log "Downloading world backup from $DOWNLOAD_LINK. This may take several minutes..."
-wget $DOWNLOAD_LINK -O "$FILE_PATH"
+downloadWorld
 
 log "Extracting world backup..."
 tar -xvf "$FILE_PATH" world
@@ -89,7 +142,7 @@ log "Uploading map to web server..."
 
 function s3_sync {
     log "Running S3 sync..."
-    /usr/local/bin/aws s3 sync --delete ./output $S3_URL #$S3_URL must include trailing slash!
+    /usr/local/bin/aws s3 sync --delete output $S3_URL #$S3_URL must include trailing slash!
     log "S3 sync completed!"
 }
 
